300432cb8eef3beb78f6db0c28bf1e85805d997a
Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-05-17 10:21:28 +0700

    Removed Async code

--- MyLogin/MainWindow.xaml.cs
+++ MyLogin/MainWindow.xaml.cs
@@ -16,32 +16,7 @@ namespace MyLogin
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            LoginButton.IsEnabled = false;
-            var task = Task.Run(() => {
-
-                Thread.Sleep(2000);
-
-                return "Login Successful!";
-            });
-
-            task.ContinueWith((t) => {
-                if (t.IsFaulted)
-                {
-                    Dispatcher.Invoke(() =>
-                    {
-                        LoginButton.Content = "Login failed!";
-                        LoginButton.IsEnabled = true;
-                    });
-                }
-                else
-                {
-                    Dispatcher.Invoke(() =>
-                    {
-                        LoginButton.Content = t.Result;
-                        LoginButton.IsEnabled = true;
-                    });
-                }
-            });
+            Thread.Sleep(10000);
         }
     }
 }
